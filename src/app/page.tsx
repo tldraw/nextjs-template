f708864efd3524e0697d346c4ec00ba8fb0a28a7
tldraw <dan+github.actions@tldraw.com>
2024-02-09 12:12:19 +0000

    Update from https://github.com/tldraw/tldraw/commit/405e431b749c2fcb43f251ece3a0a2efcf864200

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
-import { Tldraw } from "@tldraw/tldraw";
+'use client'
+import { Tldraw } from '@tldraw/tldraw'
 
 export default function Home() {
-  return (
-    <main>
-      <div style={{ position: "fixed", inset: 0 }}>
-        <Tldraw />
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div style={{ position: 'fixed', inset: 0 }}>
+				<Tldraw />
+			</div>
+		</main>
+	)
 }
